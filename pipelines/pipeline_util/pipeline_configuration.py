bf7cb03344f190f9e01078412efde676f8417a22
Mat Moore <matmoore@users.noreply.github.com>
2018-03-13 16:47:42 +0000

    Remove stale comment

--- pipelines/pipeline_util/pipeline_configuration.py
+++ pipelines/pipeline_util/pipeline_configuration.py
@@ -1,11 +1,6 @@
 class PipelineConfiguration:
     """
-    Maintains settings which are treated as constants by the rest of
-    the pipeline.  Most of the settings refer to:
-        - which date format should be used for file names and the time
-          stamp format used in logging.
-        - the names of directories that appear when the pipelines are run
-        - standardised parameter values for CSV file processing
+    Constants used by the rest of the pipeline.
     """
     DATE_OUTPUT_FORMAT = '%Y-%m-%d'
     TIME_STAMP_FORMAT = '%b %d, %Y %H.%M.%S'
